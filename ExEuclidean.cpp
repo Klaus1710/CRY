00ff7e18dbc98aa5eeb4ffc18e68fa02e797a696
Kushagra Raj Gupta <96344516+Klaus1710@users.noreply.github.com>
2023-06-07 12:43:44 +0530

    Update ExEuclidean.cpp

--- ExEuclidean.cpp
+++ ExEuclidean.cpp
@@ -1,24 +1,45 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-int gcdExtended(int a, int b, int *x1, int *y1)
+int extended_euclidean_algorithm(int a, int b, int &x, int &y)
 {
-	if (a == 0)
-	{
-		*x1 = 0;
-		*y1 = 1;
-		return b;
-	}
-	int x2, y2; 
-	int gcd = gcdExtended(b%a, a, &x2, &y2);	
-	*x1 = y2 - (b/a) * x2;
-	*y1 = x2;
-	return gcd;
+    if (b == 0)
+    {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int gcd = extended_euclidean_algorithm(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - (a / b) * y1;
+    return gcd;
+}
+int find_modular_inverse(int a, int m)
+{
+    int x, y;
+    int gcd = extended_euclidean_algorithm(a, m, x, y);
+    if (gcd != 1)
+    {
+        throw runtime_error("Inverse does not exist.");
+    }
+    return (x + m) % m;
 }
 int main()
 {
-	int x1, y1, a = 30, b = 12;
-	int g = gcdExtended(a, b, &x1, &y1);
-	cout << "GCD(" << a << ", " << b
-		<< ") = " << g << endl;
-	return 0;
+    int a, m;
+    cout << "Enter a number to find inverse: ";
+    cin >> a;
+    cout << "Enter the number whose modulus is to be found: ";
+    cin >> m;
+    int inverse;
+    try
+    {
+        inverse = find_modular_inverse(a, m);
+        cout << "Modular inverse of " << a << " mod " << m << " is: " << inverse << endl;
+    }
+    catch (const exception &e)
+    {
+        cout << "Error: " << e.what() << endl;
+    }
+    return 0;
 }
